dda32f6bd1a8e8dc86d5f5aa5c53279aed76caeb
scarl-ett <chdnjs7610@gmail.com>
2022-05-11 00:00:26 +0900

    회원가입

---
+++ src/components/common/base/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styledComponents from 'styled-components';
+
+const HeaderBlock = styledComponents.div``;
+
+const Header = () => {
+  return (
+    <HeaderBlock>
+
+    </HeaderBlock>
+  );
+};
+
+export default Header;
